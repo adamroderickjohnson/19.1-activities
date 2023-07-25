b4ae60f7976e199b857fe142e0c8807881b3abc7
adamroderickjohnson <adam.roderick.johnson@gmail.com>
2023-07-25 19:49:12 -0400

    Finished Activity 2

--- 01_Hello_World_Again/unsolved/js/index.js
+++ 01_Hello_World_Again/unsolved/js/index.js
@@ -1,5 +1,20 @@
 // Create a variable below
 var helloWorld = 'Hello World';
 var number='12345';
+var n = 400;
 // Console.log() your variable here.
-console.log(helloWorld, number)
+console.log(helloWorld, number);
+
+function adamStuff(x){
+    
+   for (let i = 0; i < x.length; i++){
+       if ( i == x/2 ) {
+        console.log('excellent');
+    } 
+       else {
+        console.log('not excellent');
+    }
+   } 
+}
+
+adamStuff(n);
--- 02_More_Variables/unsolved/js/index.js
+++ 02_More_Variables/unsolved/js/index.js
@@ -1,3 +1,21 @@
 // Create the addNumbers function here.
-
+var stringTwo = "2";
+var numTwo = 2;
 // Create the stringConcat function here.
+function addNumbers(){
+    var two = 2;
+    var three = 3;
+    var five = two + three;
+
+    console.log(five);
+}
+
+function stringConcat(){
+    var taco = 'taco';
+    var cat ='cat';
+    var newString = taco + ' ' + cat;
+    alert(newString);
+}
+
+addNumbers();
+stringConcat();
